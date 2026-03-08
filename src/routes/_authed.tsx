18ccb65da5f6301c74d8732c072dd2ade6adaa80
ChristiannYa <christian.ylopez02@gmail.com>
2026-03-07 23:56:05 -0600

    fix('_authed.tsx' access token check fixed)

--- src/routes/_authed.tsx
+++ src/routes/_authed.tsx
@@ -4,7 +4,7 @@ import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 export const Route = createFileRoute("/_authed")({
 	beforeLoad: () => {
 		const accessToken = useAccessTokenStore.getState().accessToken;
-		if (accessToken) {
+		if (!accessToken) {
 			throw redirect({ to: "/login" });
 		}
 	},
